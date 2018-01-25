c8fde70b0c7f7744fe5783e16a15c16e7275fdda
Alberto GIl <alberto8gil@gmail.com>
2018-01-25 09:59:44 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,20 +24,8 @@ def startOver():
 
 @app.route('/page1')
 def renderPage1():
-    return render_template('page1.html')
-
-@app.route('/page2',methods=['GET','POST'])
-def renderPage2():
-    #set the first and last name in the sessions
-    session["firstName"] = request.form["firstName"]
-    session["lastName"] = request.form["lastName"]
-    return render_template('page2.html')
-
-@app.route('/page3',methods=['GET','POST'])
-def renderPage3():
-    #set the favorite color in the session
-    session["favoriteColor"] = request.form["favoriteColor"]
-    return render_template('page3.html')
+  #Check answer here
+    return render_template('answerPage.html')
     
 if __name__=="__main__":
     app.run(debug=False)
